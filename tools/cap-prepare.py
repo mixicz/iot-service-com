f332abde4ba2abd8debfe18cd521df05788c3b0c
Miroslav Maiksnar <git@m.mixi.cz>
2023-07-07 23:04:59 +0200

    changed protobuf capability list from string to enum in order to save space in 50 char message

--- tools/cap-prepare.py
+++ tools/cap-prepare.py
@@ -16,37 +16,23 @@ Example:
 from pprint import pprint
 import sys
 import base64
-# import struct
-# import hashlib
-
-# from google.protobuf import text_format
 
 import interface.cap_pb2 as cap_pb2
 
 def main():
     online = sys.argv[1].lower() == 'true'
-    capabilities = sys.argv[2:]
+    capabilities = [s.upper() for s in sys.argv[2:]]
 
     cap = cap_pb2.Cap()
     cap.onl = online
     cap.acc.extend(capabilities)
-    # pprint(cap)
 
     cap_str = cap.SerializeToString()
-    # cap_str = struct.pack('<I', len(cap_str)) + cap_str
-    # cap_str = b'\x01' + cap_str
-    # pprint(cap_str)
 
     cap_b64 = base64.b64encode(cap_str).decode('ascii')
 
     print(cap_b64)
 
-    # print(text_format.MessageToString(cap))
-
-    # print(cap_str)
-    # print(cap_str.hex())
-
-    # print(hashlib.sha256(cap_str).hexdigest())
 
 if __name__ == '__main__':
     main()
--- tools/interface/cap_pb2.py
+++ tools/interface/cap_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x13interface/cap.proto\x12\x0biot_service\",\n\x03\x43\x61p\x12\x0b\n\x03onl\x18\x01 \x01(\x08\x12\x0b\n\x03\x64\x65v\x18\x02 \x01(\t\x12\x0b\n\x03\x61\x63\x63\x18\x03 \x03(\tB3Z1github.com/mixicz/iot-service-com/com-service/capb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x13interface/cap.proto\x12\x0biot_service\"B\n\x03\x43\x61p\x12\x0b\n\x03onl\x18\x01 \x01(\x08\x12\x0b\n\x03\x64\x65v\x18\x02 \x01(\t\x12!\n\x03\x61\x63\x63\x18\x03 \x03(\x0e\x32\x14.iot_service.CapData*6\n\x07\x43\x61pData\x12\x07\n\x03TIM\x10\x00\x12\x07\n\x03MET\x10\x01\x12\x07\n\x03\x42\x45L\x10\x02\x12\x07\n\x03PVC\x10\x03\x12\x07\n\x03PVD\x10\x04\x42\x33Z1github.com/mixicz/iot-service-com/com-service/capb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -22,6 +22,8 @@ if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
   DESCRIPTOR._serialized_options = b'Z1github.com/mixicz/iot-service-com/com-service/cap'
+  _globals['_CAPDATA']._serialized_start=104
+  _globals['_CAPDATA']._serialized_end=158
   _globals['_CAP']._serialized_start=36
-  _globals['_CAP']._serialized_end=80
+  _globals['_CAP']._serialized_end=102
 # @@protoc_insertion_point(module_scope)
